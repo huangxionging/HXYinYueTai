a7ca0c371e051adbc4d942fd7ea6625da061b2f7
huangxiong <huangxiong@huangxiong.local>
2014-08-30 00:52:22 +0800

    Initial Commit

---
+++ HXYinYueTai/HXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HXAppDelegate.h
+//  HXYinYueTai
+//
+//  Created by huangxiong on 14-8-30.
+//  Copyright (c) 2014年 New-Life. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HXYinYueTai/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HXYinYueTai
+//
+//  Created by huangxiong on 14-8-30.
+//  Copyright (c) 2014年 New-Life. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HXAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HXAppDelegate class]));
+    }
+}
